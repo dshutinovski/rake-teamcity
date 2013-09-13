71ac4b3507c3d01595208cf4903bd34dbde84373
Wesley McClure <wmcclure@phoenixwebgroup.com>
2013-09-13 08:59:56 -0700

    disabling some stuff pre-demo

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,9 +5,9 @@ task :default => [:teamcity]
 task :teamcity => [:build,:spec]
 
 task :build do
-	ENV.each {|key, value| puts "#{key} = #{value}" }
+	#ENV.each {|key, value| puts "#{key} = #{value}" }
 end
 
 RSpec::Core::RakeTask.new(:spec) do |t|
-  t.fail_on_error = false
+	#t.fail_on_error = false
 end

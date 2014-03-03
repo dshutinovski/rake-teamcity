e947d61efbafe8f7683b933bec2656e0d43b399e
Wesley McClure <wes.mcclure@gmail.com>
2014-03-02 19:36:51 -0800

    add test project to demo tests -rearrange src

--- dotnet/Rakefile.rb
+++ dotnet/Rakefile.rb
@@ -1,9 +1,30 @@
 require 'albacore'
 
-msbuild :build_release do |msb|
-  msb.properties :configuration => :Release
-  msb.targets :Build
-  msb.solution = 'Project.sln'
+OUTPUT_DIRECTORY = 'output'
+SOLUTION_FILE = 'src/Project.sln'
+PACKAGE_FILE = 'app.zip'
+
+task :clean do 
+  FileUtils.rm_rf(OUTPUT_DIRECTORY)
+  FileUtils.mkdir(OUTPUT_DIRECTORY)
 end
 
-task :teamcity => [:build_release]
+msbuild :build do |msb|
+  sh 'nuget restore src/'
+  msb.properties :configuration => :Release
+  msb.targets :Build
+  msb.solution = SOLUTION_FILE
+end
+
+nunit :tests => [:build] do |cmd|
+  cmd.command = "src/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe"
+  cmd.assemblies = FileList["src/Tests/bin/Release/Tests.dll"]
+end
+
+zip :package => [:build] do |zip|
+  zip.directories_to_zip = ["src/Code/bin/Release"]
+  zip.output_file = PACKAGE_FILE
+  zip.output_path = OUTPUT_DIRECTORY
+end
+
+task :teamcity => [:package]

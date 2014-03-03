6e02e3ffbe58f1bf56bf6d37a4837c27c0b7dd2f
Wesley McClure <wes.mcclure@gmail.com>
2014-03-02 22:37:08 -0800

    add quick web demo for delivery demo

--- dotnet/Rakefile.rb
+++ dotnet/Rakefile.rb
@@ -3,6 +3,7 @@ require 'albacore'
 OUTPUT_DIRECTORY = 'output'
 SOLUTION_FILE = 'src/Project.sln'
 PACKAGE_FILE = 'app.zip'
+DELIVERY_DIRECTORY = 'C:\delivery'
 
 task :clean do 
   FileUtils.rm_rf(OUTPUT_DIRECTORY)
@@ -16,15 +17,34 @@ msbuild :build do |msb|
   msb.solution = SOLUTION_FILE
 end
 
-nunit :tests => [:build] do |cmd|
+nunit :tests => [:clean, :build] do |cmd|
   cmd.command = "src/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe"
   cmd.assemblies = FileList["src/Tests/bin/Release/Tests.dll"]
 end
 
-zip :package => [:build] do |zip|
+zip :package => [:clean, :build] do |zip|
   zip.directories_to_zip = ["src/Code/bin/Release"]
   zip.output_file = PACKAGE_FILE
   zip.output_path = OUTPUT_DIRECTORY
+end
+
+task :deliver => [:package] do
+  executable = File.join(DELIVERY_DIRECTORY, 'Project.exe')
+  
+  ## uninstall service if exists
+  begin
+	sh "#{executable} uninstall"
+  rescue
+  end
+  
+  ## copy new
+  FileUtils.rm_rf(DELIVERY_DIRECTORY)
+  FileUtils.mkdir(DELIVERY_DIRECTORY)
+  archive = File.join(OUTPUT_DIRECTORY, PACKAGE_FILE)
+  sh "7z e -o#{DELIVERY_DIRECTORY} #{archive}"
+
+  ## install service
+  sh "#{executable} install start"
 end
 
 task :teamcity => [:package]

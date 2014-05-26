c014093f106be6b6a08833e93a891a5593114d06
Wesley McClure <wes.mcclure@gmail.com>
2014-05-26 13:50:00 -0700

    Add sample for code coverage reporting

---
+++ dotnet/src/Tests/CoverageTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class CoverageTests : AssertionHelper
+	{
+		[Test]
+		public void TestTrue()
+		{
+			Expect(BooleanToInt(true), Is.EqualTo(1));
+		}
+
+		public int BooleanToInt(bool value)
+		{
+			if (value)
+			{
+				return 1;
+			}
+			return 0;
+		}
+	}
+}
